3a9d7f38cef6a8efb97b74e859e7460e2e8ce715
Filip Kirin <filip.kirin.zg1@gmail.com>
2025-03-17 20:10:12 +0100

    run forge fmt

--- src/interfaces/IERC721Enumerable.sol
+++ src/interfaces/IERC721Enumerable.sol
@@ -5,7 +5,7 @@ pragma solidity 0.8.29;
 /// @title ERC-721 Non-Fungible Token Standard, optional enumeration extension
 /// @dev See https://eips.ethereum.org/EIPS/eip-721
 ///  Note: the ERC-165 identifier for this interface is 0x780e9d63.
-interface IERC721Enumerable /* is ERC721 */ {
+interface IERC721Enumerable { /* is ERC721 */
     /// @notice Count NFTs tracked by this contract
     /// @return A count of valid NFTs tracked by this contract, where each one of
     ///  them has an assigned and queryable owner not equal to the zero address
